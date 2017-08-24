80ca3bf01560c6b7de4a5acfa357f1671cd762d7
Ryan Desmond <radesmond@gmail.com>
2017-08-23 18:06:39 -0700

    adding /api/** permitAll

--- src/main/java/crypto/configuration/SecurityConfiguration.java
+++ src/main/java/crypto/configuration/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/registration").permitAll()
-                .antMatchers("/api/").permitAll()
+                .antMatchers("/api/**").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
                 .loginPage("/login").failureUrl("/login?error=true")
