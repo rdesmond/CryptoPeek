0ed7910133a2b292b63d2e8971ba4b55aa093baf
Aaron <bigelow_aaron@hotmail.com>
2017-09-15 10:44:13 -0600

    added arbitrage db insert

---
+++ src/main/java/crypto/mappers/ArbitrageMapper.java
@@ -0,0 +1,20 @@
+package crypto.mappers;
+
+import crypto.model.arbitrageModels.ArbitrageStatsForDbInsert;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Created by aaron on 9/14/17.
+ */
+@Mapper
+public interface ArbitrageMapper {
+
+    String INSERT_ARBITRAGE_STATS = "INSERT INTO cryptopeek.arbitrage_stats (symbol, difference_in_USD, " +
+            "high_exchange_name, high_exchange_price, low_exchange_name, low_exchange_price, time) VALUES " +
+            "(#{symbol}, #{differencein$USD}, #{highExchangeName}, #{highExchangePrice}, #{lowExchangeName}, " +
+            "#{lowExchangePrice}, #{time});";
+
+    @Insert(INSERT_ARBITRAGE_STATS)
+    public void addArbitrageStatsToDb(ArbitrageStatsForDbInsert arbitrageStatsForDbInsert);
+}
