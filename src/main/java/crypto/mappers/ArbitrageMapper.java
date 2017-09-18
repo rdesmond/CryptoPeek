2b4533e058e57d71d93d0738453ed0d7fdaac968
Aaron <bigelow_aaron@hotmail.com>
2017-09-18 12:13:09 -0600

    added comments

--- src/main/java/crypto/mappers/ArbitrageMapper.java
+++ src/main/java/crypto/mappers/ArbitrageMapper.java
@@ -1,5 +1,6 @@
 package crypto.mappers;
 
+import crypto.model.arbitrageModels.ArbitrageModel;
 import crypto.model.arbitrageModels.ArbitrageStatsForDbInsert;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -11,10 +12,10 @@ import org.apache.ibatis.annotations.Mapper;
 public interface ArbitrageMapper {
 
     String INSERT_ARBITRAGE_STATS = "INSERT INTO cryptopeek.arbitrage_stats (symbol, difference_in_USD, " +
-            "high_exchange_name, high_exchange_price, low_exchange_name, low_exchange_price, time) VALUES " +
+            "high_exchange_name, high_exchange_price, low_exchange_name, low_exchange_price) VALUES " +
             "(#{symbol}, #{differencein$USD}, #{highExchangeName}, #{highExchangePrice}, #{lowExchangeName}, " +
-            "#{lowExchangePrice}, #{time});";
+            "#{lowExchangePrice})";
 
     @Insert(INSERT_ARBITRAGE_STATS)
-    public void addArbitrageStatsToDb(ArbitrageStatsForDbInsert arbitrageStatsForDbInsert);
+    public void addArbitrageStatsToDb(ArbitrageModel arbitrageModel);
 }
--- src/main/java/crypto/services/ArbitrageService.java
+++ src/main/java/crypto/services/ArbitrageService.java
@@ -33,8 +33,17 @@ public class ArbitrageService {
     @Autowired
     ArbitrageMapper arbitrageMapper;
 
-    //ryan branch
-    //Aaron
+    /**
+     *
+     * @param fsym
+     * @param tsym
+     * @return arbitragemodel
+     * @throws ExchangeNotFoundException
+     *
+     * This method calculates the price difference(arbitrage opportunity) between the lowest and highest exhcnage for
+     * a given cryptocurrency.
+     *
+     */
     public ArbitrageModel getDifferenceHighestandLowestUSD (String fsym, String tsym) throws ExchangeNotFoundException {
         Double dif;
         String url = "https://www.cryptocompare.com/api/data/coinsnapshot/?fsym=" + fsym + "&tsym=" + tsym;
@@ -43,8 +52,9 @@ public class ArbitrageService {
         ArbitrageModel arbitrageModel = new ArbitrageModel();
 
         try {
+            // using the getForObject method to map the json response to cryptomodel
             cryptoModel = restTemplate.getForObject(url, CryptoModel.class);
-
+            // check to see if there are exchanges for the given crypotcurrency
             if (cryptoModel.getData().getExchanges().length < 1){
                 throw new APIUnavailableException();
             }
@@ -56,7 +66,8 @@ public class ArbitrageService {
 
             Exchanges highestExchange = null;
             Exchanges lowestExchange = null;
-
+            // looping through all the exchanges for a given currency to find the valid exchanges, then assigning
+            // the first valid exchange to the highest and lowest
             for (Exchanges e : ex){
                 if (isValidExchange(e.getMarket())){
                     highestExchange = e;
@@ -64,7 +75,6 @@ public class ArbitrageService {
                     break;
                 }
             }
-
 
             for (int i = 0 ; i < ex.length ; i++){
 
@@ -75,6 +85,7 @@ public class ArbitrageService {
                 }
 
             }
+            // assigning the first item in the array the highest exchange
             result[0] = highestExchange;
 
             for (int i = 0 ; i < ex.length ; i++){
@@ -86,8 +97,9 @@ public class ArbitrageService {
                 }
 
             }
+            // assigning the second item in the array the lowest exchange
             result[1] = lowestExchange;
-
+            // calculating the price difference between the highest and lowest exchange
             dif = Double.parseDouble(highestExchange.getPrice()) - Double.parseDouble(lowestExchange.getPrice());
             arbitrageModel.setLowExchangePrice(lowestExchange.getPrice());
             arbitrageModel.setHighExchangePrice(highestExchange.getPrice());
@@ -104,6 +116,17 @@ public class ArbitrageService {
         }
     }
 
+    /**
+     *
+     * @param fsym
+     * @param tsym
+     * @return arbitragemodel
+     * @throws ExchangeNotFoundException
+     *
+     * This method is the same as the previous method with one difference being the use of the "getBTCPrice" object to
+     * return the price differnce in USD and not BitCoin.
+     *
+     */
     public ArbitrageModel getDifferenceHighestandLowestBTC (String fsym, String tsym) throws ExchangeNotFoundException {
         Double dif;
         String url = "https://www.cryptocompare.com/api/data/coinsnapshot/?fsym=" + fsym + "&tsym=" + tsym;
@@ -173,7 +196,10 @@ public class ArbitrageService {
         }
     }
 
-    //Aaron
+    /**
+     * @author Aaron
+     * @return retVal - an arraylist of Strings from the topCoinsMapper
+     */
     public ArrayList<String> getTopCoinsArrayList(){
         ArrayList<SymbolList> coinSymbolArrayList = new ArrayList<>();
         coinSymbolArrayList = topCoinsMapper.getCoinSymbols();
@@ -184,7 +210,13 @@ public class ArbitrageService {
         return retVal;
     }
 
-    //Aaron
+    /**
+     * @author Aaron
+     * @return retVal - an arraylist of arbitragemodels
+     *
+     * This method iterates through the 30 symbols in topCoins and calls the method that returns the arbitragemodel
+     * with the price difference calculated.
+     */
     public ArrayList<ArbitrageModel> getTopArbitrageOps(){
 
         ArrayList<String> topCoins = getTopCoinsArrayList();
@@ -195,8 +227,6 @@ public class ArbitrageService {
                 try {
                     ArbitrageModel arbitrageModel = getDifferenceHighestandLowestUSD(symbol, "USD");
                     topFiveArbitrageModels.add(arbitrageModel);
-                    ArbitrageStatsForDbInsert arbitrageStatsForDbInsert = new ArbitrageStatsForDbInsert(arbitrageModel);
-                    arbitrageMapper.addArbitrageStatsToDb(arbitrageStatsForDbInsert);
                 } catch (ExchangeNotFoundException e) {
                     e.printStackTrace();
                 }
@@ -204,19 +234,20 @@ public class ArbitrageService {
                 try {
                     ArbitrageModel arbitrageModel = getDifferenceHighestandLowestBTC(symbol,"BTC");
                     topFiveArbitrageModels.add(arbitrageModel);
-                    ArbitrageStatsForDbInsert arbitrageStatsForDbInsert = new ArbitrageStatsForDbInsert(arbitrageModel);
-                    arbitrageMapper.addArbitrageStatsToDb(arbitrageStatsForDbInsert);
                 } catch (ExchangeNotFoundException e) {
                     e.printStackTrace();
                 }
             }
         }
-
         ArrayList<ArbitrageModel> retVal = new ArrayList<ArbitrageModel>();
+        //The following sorts the topFiveArbitrageModels arraylist
         Collections.sort(topFiveArbitrageModels, (o1, o2) -> o1.getDifferencein$USD().compareTo(o2.getDifferencein$USD()));
+        //The for loop below iterates through from topFiveArbitrageModels.size to zero and finds the first five (biggest)
+        //price differences and adds them to a new array called retVal.
         for (int i = topFiveArbitrageModels.size(); retVal.size() < 5 && i > 0 ; i--){
             if (!(topFiveArbitrageModels.get(i-1).getDifferencein$USD() == 0)){
                 retVal.add(topFiveArbitrageModels.get(i-1));
+                arbitrageMapper.addArbitrageStatsToDb(topFiveArbitrageModels.get(i-1));
             }
 
         }
@@ -230,7 +261,13 @@ public class ArbitrageService {
 
     }
 
-    //Aaron
+    /**
+     * @author Aaron
+     * @param exchange
+     * @return boolean
+     *
+     * The following method checks to see if the the exchange is valid based upon a check of five exchanges
+     */
     public boolean isValidExchange(String exchange){
         if (exchange.equalsIgnoreCase("Coinbase")){
             return true;
